f740d5e45f0c1c79297cb74c66b60b009866cec5
Jorge Henry Carmona Alzate <jorgecarmonasig@gmail.com>
2026-08-07 13:53:36 -0500

    Agregar experiencia, contacto y diseño responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
-console.log("Renacer 62");
+const menuButton = document.querySelector('.header__toggle');
+const navigation = document.querySelector('.header__nav');
+const navigationLinks = document.querySelectorAll('.header__nav a');
+
+menuButton.addEventListener('click', () => {
+
+    const menuIsOpen = navigation.classList.toggle('is-open');
+
+    menuButton.setAttribute('aria-expanded', menuIsOpen);
+
+});
+
+navigationLinks.forEach(link => {
+
+    link.addEventListener('click', () => {
+
+        navigation.classList.remove('is-open');
+        menuButton.setAttribute('aria-expanded', 'false');
+
+    });
+
+});
